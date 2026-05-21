c20ecf7b63cdc164c496ed0258e0005c01b2b097
Falmines <fakhruddinnaufal2003@gmail.com>
2026-05-21 18:45:21 +0700

    fix url

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
+console.log("DB:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
